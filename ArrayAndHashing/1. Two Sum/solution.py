2af9c600a7346d15573bc9bc7cf7b36921aa6d52
Sherry Huang <sherry03210321@gmail.com>
2025-07-19 17:42:46 +0800

    Update solution.py

--- ArrayAndHashing/1. Two Sum/solution.py
+++ ArrayAndHashing/1. Two Sum/solution.py
@@ -1,1 +1,18 @@
+# Use sort
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j] 
+                  
+# Implement the code by using a hash map(enumerate):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        preMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in preMap:
+                return [preMap[diff], i]
+            preMap[n] = i
 
