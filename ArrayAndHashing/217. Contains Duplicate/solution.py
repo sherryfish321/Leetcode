3fc23f560c4ea86fbb8ccedaacc0c117ec13f440
Sherry Huang <sherry03210321@gmail.com>
2025-07-20 22:42:00 +0800

    Update solution.py

--- ArrayAndHashing/217. Contains Duplicate/solution.py
+++ ArrayAndHashing/217. Contains Duplicate/solution.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        preMap = {}
+        for i, n in enumerate(nums):
+            if n in preMap:
+                return True
+            preMap[n] = i
+        return False
+      
+# improve the code by using hash set
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+        
