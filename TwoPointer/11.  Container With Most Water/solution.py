0bfeaf9218691978d00b1d0f4c38e21b50ebe485
Sherry Huang <sherry03210321@gmail.com>
2025-08-03 00:47:46 -0700

    Update solution.py

--- TwoPointer/11.  Container With Most Water/solution.py
+++ TwoPointer/11.  Container With Most Water/solution.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l = 0
+        r = len(heights) - 1
+        max_area = 0
+        while l < r:
+            width = r - l
+            height = min(heights[l], heights[r])
+            # Current container area
+            area = width * height
+            # Update max_area if the current one is greater
+            max_area = max(area, max_area)
+            # Move the pointer to the shorter line inward
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
