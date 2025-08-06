06d7b2a2ab95a6d79c693e9c60bf5d6bee3536c0
Sherry Huang <sherry03210321@gmail.com>
2025-08-06 12:50:53 -0700

    Update solution.py

--- Stack/150. Evaluate Reverse Polish Notation/solution.py
+++ Stack/150. Evaluate Reverse Polish Notation/solution.py
@@ -1,1 +1,23 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in "+-*/":
+                # Pop the two operands from the stack 
+                b = stack.pop()
+                a = stack.pop()
+                # Apply the operator to a & b
+                if token == "+":
+                    stack.append(a + b)
+                elif token == "-":
+                    stack.append(a - b)
+                elif token == "*":
+                    stack.append(a * b)
+                # Perform integer division with truncation toward zero
+                else:
+                    stack.append(int(a / b))
+            else:
+                # Convert numeric string to integer and push onto stack
+                stack.append(int(token))
+        return stack[0]
 
