02e28b90e420a825c7a297806b6bd725d1cde3c2
Sherry Huang <sherry03210321@gmail.com>
2025-08-30 15:42:32 -0400

    Create solution.py

---
+++ LinkedList/2. Add Two Numbers/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            # Safely read current digit; use 0 when a list has been exhausted
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10 # Next carry
+            val = val % 10 # Current result digit
+            cur.next = ListNode(val) # Append the current digit as a new node in the result list
+
+            # update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next # The actual head of the result is next to the list
